15da558716622b3492a1a028f768509a6c36917d
Vrajpal Jhala <jhalavrajpal@gmail.com>
2026-09-20 01:01:38 +0530

    style: run prettier on Screencast.vue

--- docs/.vitepress/theme/components/Screencast.vue
+++ docs/.vitepress/theme/components/Screencast.vue
@@ -72,7 +72,11 @@ function formatTime(seconds: number) {
 
 <template>
   <div class="screencast" @mousemove="started && showControls()">
-    <img :src="poster" :alt="alt ?? 'Screencast preview'" class="screencast-media" />
+    <img
+      :src="poster"
+      :alt="alt ?? 'Screencast preview'"
+      class="screencast-media"
+    />
 
     <video
       v-show="started"
@@ -97,19 +101,46 @@ function formatTime(seconds: number) {
       @timeupdate="onTimeUpdate"
     />
 
-    <button v-if="!started" type="button" class="screencast-play" aria-label="Play video" @click="start">
-      <svg viewBox="0 0 24 24" width="22" height="22" fill="currentColor"><path d="M8 5v14l11-7z" /></svg>
+    <button
+      v-if="!started"
+      type="button"
+      class="screencast-play"
+      aria-label="Play video"
+      @click="start"
+    >
+      <svg viewBox="0 0 24 24" width="22" height="22" fill="currentColor">
+        <path d="M8 5v14l11-7z" />
+      </svg>
     </button>
 
     <div v-if="!started && label" class="screencast-caption">{{ label }}</div>
 
     <!-- ponytail: controls stay visible once started; add auto-hide-on-idle if the bar gets distracting -->
     <div v-if="started" class="screencast-controls">
-      <button type="button" class="screencast-btn" :aria-label="isPlaying ? 'Pause' : 'Play'" @click="togglePlay">
-        <svg v-if="isPlaying" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+      <button
+        type="button"
+        class="screencast-btn"
+        :aria-label="isPlaying ? 'Pause' : 'Play'"
+        @click="togglePlay"
+      >
+        <svg
+          v-if="isPlaying"
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          fill="currentColor"
+        >
           <path d="M6 5h4v14H6zM14 5h4v14h-4z" />
         </svg>
-        <svg v-else viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M8 5v14l11-7z" /></svg>
+        <svg
+          v-else
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          fill="currentColor"
+        >
+          <path d="M8 5v14l11-7z" />
+        </svg>
       </button>
 
       <input
@@ -123,24 +154,50 @@ function formatTime(seconds: number) {
         @input="onSeek"
       />
 
-      <span class="screencast-time">{{ formatTime(currentTime) }} / {{ formatTime(duration) }}</span>
+      <span class="screencast-time"
+        >{{ formatTime(currentTime) }} / {{ formatTime(duration) }}</span
+      >
 
-      <button type="button" class="screencast-btn" :aria-label="isMuted ? 'Unmute' : 'Mute'" @click="toggleMute">
-        <svg v-if="isMuted" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+      <button
+        type="button"
+        class="screencast-btn"
+        :aria-label="isMuted ? 'Unmute' : 'Mute'"
+        @click="toggleMute"
+      >
+        <svg
+          v-if="isMuted"
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          fill="currentColor"
+        >
           <path
             d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.42.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3 3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06a8.99 8.99 0 0 0 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4 9.91 6.09 12 8.18V4z"
           />
         </svg>
-        <svg v-else viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+        <svg
+          v-else
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          fill="currentColor"
+        >
           <path
             d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"
           />
         </svg>
       </button>
 
-      <button type="button" class="screencast-btn" aria-label="Fullscreen" @click="toggleFullscreen">
+      <button
+        type="button"
+        class="screencast-btn"
+        aria-label="Fullscreen"
+        @click="toggleFullscreen"
+      >
         <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
-          <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
+          <path
+            d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"
+          />
         </svg>
       </button>
     </div>
